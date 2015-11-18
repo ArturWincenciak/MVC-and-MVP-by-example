c7c618a007cf15c1c03e2ee497f5159b2bfa0b13
Artur Wincenciak <teo.vincent.com@gmail.com>
2015-11-18 23:22:57 +0100

    Making Active MVC more Active by adding a task in Model.

---
+++ Active MVC not in ASP.NET/Models/IObservable.cs
@@ -0,0 +1,9 @@
+﻿using TeoVincent.ActiveMVC.Views;
+
+namespace TeoVincent.ActiveMVC.Models
+{
+    public interface IObservable
+    {
+        void Subscribe(IObserver observer);
+    }
+}
--- Active MVC not in ASP.NET/Models/Model.cs
+++ Active MVC not in ASP.NET/Models/Model.cs
@@ -1,15 +1,26 @@
-﻿namespace TeoVincent.ActiveMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TeoVincent.ActiveMVC.Views;
+
+namespace TeoVincent.ActiveMVC.Models
 {
-    public class Model : Observable, IModel
+    public class Model : IModel, IObservable
     {
         private readonly int minX;
         private readonly int maxX;
 
         private readonly int minY;
         private readonly int maxY;
-        
+
         private int x;
         private int y;
+
+        private readonly List<IObserver> observers;
+        private readonly Task overAndOverAgain;
+        private Action action;
+        private const int INTERVAL = 50;
+        private readonly object lockObj;
 
         public Model(int minX, int maxX, int minY, int maxY)
         {
@@ -17,17 +28,46 @@
             this.maxX = maxX;
             this.minY = minY;
             this.maxY = maxY;
+
+            observers = new List<IObserver>();
+
+            lockObj = new object();
+            action = () => { };
+            overAndOverAgain = Task.Factory.StartNew(OverAndOverAgain);
         }
 
-        protected override void Notify()
+        public void Subscribe(IObserver observer)
         {
-            foreach (var observer in observers)
-                observer.Update(x, y);
+            observers.Add(observer);
         }
 
         public void IncreaseX()
         {
-            if (x == maxX-1)
+            lock (lockObj)
+                action = IncX;
+        }
+
+        public void DecreaseX()
+        {
+            lock (lockObj)
+                action = DecX;
+        }
+
+        public void IncreaseY()
+        {
+            lock (lockObj)
+                action = IncY;
+        }
+
+        public void DecreaseY()
+        {
+            lock (lockObj)
+                action = DecY;
+        }
+
+        private void IncX()
+        {
+            if (x == maxX - 1)
                 x = 0;
             else
                 x++;
@@ -35,19 +75,19 @@
             Notify();
         }
 
-        public void DecreaseX()
+        private void DecX()
         {
             if (x == minX)
-                x = maxX-1;
+                x = maxX - 1;
             else
                 x--;
 
             Notify();
         }
 
-        public void IncreaseY()
+        private void IncY()
         {
-            if (y == maxY-1)
+            if (y == maxY - 1)
                 y = 0;
             else
                 y++;
@@ -55,14 +95,32 @@
             Notify();
         }
 
-        public void DecreaseY()
+        private void DecY()
         {
             if (y == minY)
-                y = maxY-1;
+                y = maxY - 1;
             else
                 y--;
 
             Notify();
+        }
+
+        private void Notify()
+        {
+            foreach (var observer in observers)
+                observer.Update(x, y);
+        }
+
+        private void OverAndOverAgain()
+        {
+            while (true)
+            {
+                lock (lockObj)
+                {
+                    action();
+                    overAndOverAgain.Wait(INTERVAL);
+                }
+            }
         }
     }
 }
